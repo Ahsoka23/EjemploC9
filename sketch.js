17862508158d0f61e24aba5b229bb247ff781510
Ahsoka23 <89179461+Ahsoka23@users.noreply.github.com>
2022-02-19 13:13:39 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+
+var a; 
+function setup() {
+  createCanvas(400, 400);
+}
+
+
+function draw() {
+  background(220);
+  fill("blue");
+  rect(200,200,10,10);
+
+  fill("purple");
+  rect(200,300,10,10);
+  
+}
